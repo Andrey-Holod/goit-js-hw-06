396139800e5f1b7d64634ef71c2a0276e420ed82
Andrey-Holod <andreyholod228@icloud.com>
2023-07-05 23:23:57 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,22 +1,15 @@
+const widget = document.querySelector('.widget');
+const colorSpan = widget.querySelector('.color');
+const changeColorBtn = widget.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
-const form = document.querySelector('.login-form');
-
-form.addEventListener('submit', event => {
-  event.preventDefault();
-
-  const formData = new FormData(event.target);
-  const email = formData.get('email');
-  const password = formData.get('password');
-
-  if (email.trim() === '' || password.trim() === '') {
-    alert('Please fill in all fields');
-    return;
-  }
-
-  console.log('Login successful');
-  event.target.reset();
-});
