2f79951b9f3d3fde9ad7499fdb2f4e543457d216
Andrey-Holod <andreyholod228@icloud.com>
2023-07-05 23:29:04 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -9,7 +9,5 @@ changeColorBtn.addEventListener('click', () => {
 });
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, '0')}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
